2e1be2f3013f9a88d85028eda42c8c49ba8ce32f
preethimalathi <32563208+preethimalathi@users.noreply.github.com>
2018-11-27 12:28:26 +0530

    Add files via upload

---
+++ Project2backend/src/main/java/com/niit/dao/JobDao.java
@@ -0,0 +1,13 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import com.niit.models.Job;
+
+public interface JobDao{
+	void saveJob(Job job);
+	List<Job> getAllJobs();
+	void deleteJob(int id);
+	void updateJob(Job job);
+	Job getJob(int id);
+}
---
+++ Project2backend/src/main/java/com/niit/dao/ProfilePricuteDaoImpl.java
@@ -0,0 +1,29 @@
+package com.niit.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.niit.models.ProfilePicture;
+@Repository
+@Transactional
+public class ProfilePricuteDaoImpl implements ProfilePictureDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	public void uploadProfilePicture(ProfilePicture profilePicture) {
+		// TODO Auto-generated method stub
+		Session session=sessionFactory.getCurrentSession();
+		session.saveOrUpdate(profilePicture);
+	}
+
+	public ProfilePicture getProfilePicture(String email) {
+		// TODO Auto-generated method stub
+		Session session=sessionFactory.getCurrentSession();
+		ProfilePicture profilePicture=(ProfilePicture) session.get(ProfilePicture.class, email);
+		return profilePicture;
+	}
+
+}
